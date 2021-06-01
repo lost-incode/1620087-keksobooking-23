a400e8504587311ca08f48760420acbcbd289a96
lost-incode <vicktoriaap@gmail.com>
2021-06-01 13:04:30 +0300

    Fix functions again

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
 const getRandomInt = (min, max) => {
-  if ((min < 0) && (max > 0)) {
-    min = 0;
-  }
-  if (min === max) {
-    return min;
-  } else if ((min < 0) && (max <= 0)) {
+  if (min < 0 || max < 0) {
     return -1;
   }
-  return Math.floor(Math.random() * (Math.abs(max - min) + 1) + Math.min(min, max)); // Taken from https://developer.mozilla.org
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 getRandomInt(1, 90);
 
 const getRandomFloat = (min, max, symbolNumber) => {
-  if ((min < 0) && (max > 0)) {
-    min = 0;
-  }
-  if ((min < 0) && (max <= 0)) {
+  if (min < 0 || max < 0) {
     return -1;
-  } else if (min === max) {
-    return min.toFixed(symbolNumber);
   }
-  return (Math.random() * (Math.abs(max - min) + 0.01 ** symbolNumber) + Math.min(min, max)).toFixed(symbolNumber); // Taken from https://developer.mozilla.org
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return (Math.random() * (max - min + 0.01 ** symbolNumber) + min).toFixed(symbolNumber);
 };
 
 getRandomFloat(1, 90, 10);
