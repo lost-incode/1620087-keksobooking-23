1fdb4535823d7e07e36e0c78bf4d5b4e66906252
lost-incode <vicktoriaap@gmail.com>
2021-06-22 12:26:49 +0300

    Fix functions as criteria D23

--- js/form.js
+++ js/form.js
@@ -1,31 +1,31 @@
 const deactivateForm = () => {
   const adForm = document.querySelector('.ad-form');
   adForm.classList.add('ad-form--disabled');
-  const elementsAdForm = adForm.elements;
-  for (const element of elementsAdForm) {
-    element.setAttribute('disabled', '');
-  }
+  const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
+  const selectsAdForm = adForm.querySelectorAll('select');
+  fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = true);
+  selectsAdForm.forEach((select) => select.disabled = true);
   const mapFiltersForm = document.querySelector('.map__filters');
-  mapFiltersForm.classList.add('ad-form--disabled');
-  const elementsMapFiltersForm = mapFiltersForm.elements;
-  for (const element of elementsMapFiltersForm) {
-    element.setAttribute('disabled', '');
-  }
+  mapFiltersForm.classList.add('map__filters--disabled');
+  const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
+  const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
+  fieldsetsMapFiltersForm.forEach((fieldset) => fieldset.disabled = true);
+  selectsMapFiltersForm.forEach((select) => select.disabled = true);
 };
 
 const activateForm = () => {
   const adForm = document.querySelector('.ad-form');
   adForm.classList.remove('ad-form--disabled');
-  const elementsAdForm = adForm.elements;
-  for (const element of elementsAdForm) {
-    element.removeAttribute('disabled', '');
-  }
+  const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
+  const selectsAdForm = adForm.querySelectorAll('select');
+  fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = false);
+  selectsAdForm.forEach((select) => select.disabled = false);
   const mapFiltersForm = document.querySelector('.map__filters');
-  mapFiltersForm.classList.add('ad-form--disabled');
-  const elementsMapFiltersForm = mapFiltersForm.elements;
-  for (const element of elementsMapFiltersForm) {
-    element.removeAttribute('disabled', '');
-  }
+  mapFiltersForm.classList.remove('map__filters--disabled');
+  const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
+  const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
+  fieldsetsMapFiltersForm.forEach((fieldset) => fieldset.disabled = false);
+  selectsMapFiltersForm.forEach((select) => select.disabled = false);
 };
 
 export {deactivateForm, activateForm};
