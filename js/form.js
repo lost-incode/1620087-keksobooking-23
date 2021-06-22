cf28811a3dfdd49762f9691b0f57fd39395665bc
lost-incode <vicktoriaap@gmail.com>
2021-06-22 22:29:18 +0300

    Remove loop with selects

--- js/form.js
+++ js/form.js
@@ -1,26 +1,20 @@
 const adForm = document.querySelector('.ad-form');
 const mapFiltersForm = document.querySelector('.map__filters');
+const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
+const elementsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset, select');
 
 const deactivateForm = () => {
   adForm.classList.add('ad-form--disabled');
-  const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
   fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = true);
   mapFiltersForm.classList.add('map__filters--disabled');
-  const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
-  const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
-  fieldsetsMapFiltersForm.forEach((fieldset) => fieldset.disabled = true);
-  selectsMapFiltersForm.forEach((select) => select.disabled = true);
+  elementsMapFiltersForm.forEach((element) => element.disabled = true);
 };
 
 const activateForm = () => {
   adForm.classList.remove('ad-form--disabled');
-  const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
   fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = false);
   mapFiltersForm.classList.remove('map__filters--disabled');
-  const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
-  const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
-  fieldsetsMapFiltersForm.forEach((fieldset) => fieldset.disabled = false);
-  selectsMapFiltersForm.forEach((select) => select.disabled = false);
+  elementsMapFiltersForm.forEach((element) => element.disabled = false);
 };
 
 export {deactivateForm, activateForm};
