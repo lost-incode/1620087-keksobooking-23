0839af1843623cdd5c9b83396d21c4a3a0aad1ea
lost-incode <vicktoriaap@gmail.com>
2021-06-22 22:05:26 +0300

    Add global constants in form.js

--- js/form.js
+++ js/form.js
@@ -1,11 +1,10 @@
+const adForm = document.querySelector('.ad-form');
+const mapFiltersForm = document.querySelector('.map__filters');
+
 const deactivateForm = () => {
-  const adForm = document.querySelector('.ad-form');
   adForm.classList.add('ad-form--disabled');
   const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
-  const selectsAdForm = adForm.querySelectorAll('select');
   fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = true);
-  selectsAdForm.forEach((select) => select.disabled = true);
-  const mapFiltersForm = document.querySelector('.map__filters');
   mapFiltersForm.classList.add('map__filters--disabled');
   const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
   const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
@@ -14,13 +13,9 @@ const deactivateForm = () => {
 };
 
 const activateForm = () => {
-  const adForm = document.querySelector('.ad-form');
   adForm.classList.remove('ad-form--disabled');
   const fieldsetsAdForm = adForm.querySelectorAll('fieldset');
-  const selectsAdForm = adForm.querySelectorAll('select');
   fieldsetsAdForm.forEach((fieldset) => fieldset.disabled = false);
-  selectsAdForm.forEach((select) => select.disabled = false);
-  const mapFiltersForm = document.querySelector('.map__filters');
   mapFiltersForm.classList.remove('map__filters--disabled');
   const fieldsetsMapFiltersForm = mapFiltersForm.querySelectorAll('fieldset');
   const selectsMapFiltersForm = mapFiltersForm.querySelectorAll('select');
