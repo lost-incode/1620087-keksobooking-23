bb6470b5348d563cf4bc804130d556dcff1dbb39
lost-incode <vicktoriaap@gmail.com>
2021-07-12 14:24:51 +0300

    Add function for show preview of hostel photoin adForm

--- js/user-form.js
+++ js/user-form.js
@@ -3,6 +3,36 @@ import {mainPinMarker, map} from './map.js';
 import {LAT_DEFAULT, LNG_DEFAULT} from './data.js';
 import {request} from './api.js';
 const adForm = document.querySelector('.ad-form');
+const adFormPhoto = adForm.querySelector('.ad-form__photo');
+const adFormPhotoInput = adForm.querySelector('#images');
+
+const previewFile = () => {
+  if (adFormPhoto.firstChild) {
+    adFormPhoto.removeChild(adFormPhoto.firstChild);
+  }
+  const image = document.createElement('img');
+  image.width = 70;
+  image.height = 70;
+  const file = adFormPhotoInput.files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener('load', () => {
+    // convert image file to base64 string
+    image.src= reader.result;
+  }, false);
+
+  reader.readAsDataURL(file);
+  adFormPhoto.appendChild(image);
+};
+
+adFormPhotoInput.addEventListener('change', () => previewFile());
+
+// adFormPhotoInput.addEventListener('change', (evt) => {
+//   const fileList = evt.target.files;
+//   const image = document.createElement('img');
+//   image.title = `${escape(fileList[0].name)}`;
+//   image.src=`${evt.target.result}`;
+
 
 const setUserFormSubmit = (onSuccess, onError) => {
   adForm.addEventListener('submit', (evt) => {
